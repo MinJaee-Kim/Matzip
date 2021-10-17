cacd8d42e6c3321a4c423ea3e683c0d307b74e60
yt90as <yt90as@naver.com>
2021-10-17 17:29:24 +0900

    Update BoardPresenter.java

--- app/src/main/java/kr/ac/uc/matzip/presenter/BoardPresenter.java
+++ app/src/main/java/kr/ac/uc/matzip/presenter/BoardPresenter.java
@@ -2,14 +2,13 @@ package kr.ac.uc.matzip.presenter;
 
 import kr.ac.uc.matzip.view.MainActivity;
 
-public class BoardPresenter {
+public class BoardPresenter{
     private MainActivity mainActivity;
-
     BoardPresenter(MainActivity mainActivity){
         this.mainActivity = mainActivity;
     }
-
     public void onDestroy(){
         mainActivity = null;
     }
+
 }
