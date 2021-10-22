b36a6a02adb11fa7f13efb15f48fc1bc3340ff29
Usofl <songjang33@naver.com>
2021-10-22 22:46:11 +0900

    d

--- app/src/main/java/kr/ac/uc/matzip/view/MainActivity.java
+++ app/src/main/java/kr/ac/uc/matzip/view/MainActivity.java
@@ -1,26 +1,11 @@
 package kr.ac.uc.matzip.view;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.security.MessageDigest;
 
 import kr.ac.uc.matzip.R;
 import kr.ac.uc.matzip.model.BoardModel;
@@ -62,6 +47,4 @@ public class MainActivity extends AppCompatActivity implements BoardModel.View {
             }
         });
     }
-
-
 }
