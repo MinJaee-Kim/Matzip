6a15bc475081b5c99e5fc6c96e3bba3e04613833
Usofl <songjang33@naver.com>
2021-10-27 16:20:39 +0900

    123

--- app/src/main/java/kr/ac/uc/matzip/model/BoardModel.java
+++ app/src/main/java/kr/ac/uc/matzip/model/BoardModel.java
@@ -1,5 +1,52 @@
 package kr.ac.uc.matzip.model;
 
-public class BoardModel {
+import com.google.gson.annotations.SerializedName;
 
+public class BoardModel {
+    @SerializedName("id")
+    int id;
+
+    @SerializedName("username")
+    int username;
+
+    @SerializedName("bo_title")
+    String bo_title;
+
+    @SerializedName("bo_cont")
+    String bo_cont;
+
+
+    @SerializedName("nowdate")
+    String nowdate; // mysql에서 datetime은 문자열로 전송된다.
+
+    @SerializedName("bo_love")
+    int bo_love;
+
+    @SerializedName("viewcount")
+    int viewcount;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getBo_title() {
+        return bo_title;
+    }
+
+    public String getBo_cont() {
+        return bo_cont;
+    }
+
+    public String getNowdate() {
+        return nowdate;
+    }
+
+    public int getBo_love() {
+        return bo_love;
+    }
+
+    public Integer getViewcount() {
+        return viewcount;
+    }
 }
---
+++ app/src/main/java/kr/ac/uc/matzip/presenter/BoardAPI.java
@@ -0,0 +1,14 @@
+package kr.ac.uc.matzip.presenter;
+
+import kr.ac.uc.matzip.model.BoardModel;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface BoardAPI {
+    @FormUrlEncoded
+    @POST("insert_Board.php")
+    Call<BoardModel> savaLike(@Field("bo_title") String bo_title,
+                              @Field("bo_cont") String bo_cont);
+}
