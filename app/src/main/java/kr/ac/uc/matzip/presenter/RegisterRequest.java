c485f7b51adae7951737023f438bfd1a8237eddc
Usofl <songjang33@naver.com>
2021-10-27 15:21:58 +0900

    쿨한커밋 회원가입 완료

--- app/src/main/java/kr/ac/uc/matzip/presenter/RegisterRequest.java
+++ app/src/main/java/kr/ac/uc/matzip/presenter/RegisterRequest.java
@@ -14,14 +14,13 @@ public class RegisterRequest extends StringRequest {
     private Map<String, String> map;
 
 
-    public RegisterRequest(String username, String password, Response.Listener<String> listener) {
+    public RegisterRequest(String username, String password, String nickname,Response.Listener<String> listener) {
         super(Method.POST ,URL, listener, null);
 
         map = new HashMap<>();
         map.put("username", username);
         map.put("password", password);
-//        map.put("authority", nickname);
-//        map.put("enabled", nickname);
+        map.put("nickname", nickname);
     }
 
     @Override
