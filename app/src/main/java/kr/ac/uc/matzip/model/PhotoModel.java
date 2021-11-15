4c916dcd8a39b0e15b1f6245c847c5970cad5477
Usofl <songjang33@naver.com>
2021-11-16 02:42:47 +0900

    Update PhotoModel.java

--- app/src/main/java/kr/ac/uc/matzip/model/PhotoModel.java
+++ app/src/main/java/kr/ac/uc/matzip/model/PhotoModel.java
@@ -3,8 +3,6 @@ package kr.ac.uc.matzip.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import okhttp3.RequestBody;
-
 public class PhotoModel {
     @Expose
     @SerializedName("id")
