7f0c1498c14ba17f37553717d25ed25ec5673cb4
yt90as <yt90as@naver.com>
2021-11-25 16:16:02 +0900

    일단 커밋
    ㅇㅇ

--- app/src/main/java/kr/ac/uc/matzip/view/BottomSheetFragment.java
+++ app/src/main/java/kr/ac/uc/matzip/view/BottomSheetFragment.java
@@ -25,9 +25,9 @@ import kr.ac.uc.matzip.R;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
 
-    private Button bs_photo, bs_mkboard;
-    private TextView bs_address;
-    private EditText bs_title, bs_cont;
+    private ImageView bs_photo;
+    private Button bs_checkBtn, bs_locationBtn;
+    private EditText bs_title, bs_cont, bs_address;
     private View contentView;
     private BottomSheetBehavior mBehavior;
 
@@ -40,7 +40,7 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
 
     @Override   //여기서 dialog를 생성합니다.
     public void setupDialog(@NonNull Dialog dialog, int style) {    //onCreateView 이전에 불립니다.
-        contentView = View.inflate(getContext(), R.layout.bottom_sheet, null);
+        contentView = View.inflate(getContext(), R.layout.bottom_sheet_design, null);
         dialog.setContentView(contentView);
         //여기의 메서드를 사용해서 크기를 조절
         mBehavior = BottomSheetBehavior.from((View) contentView.getParent());
@@ -61,18 +61,20 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        View view = inflater.inflate(R.layout.bottom_sheet_design, container, false);
 
-        bs_photo = view.findViewById(R.id.bs_btnPhoto);
-        bs_mkboard = view.findViewById(R.id.bs_mkBtn);
-        bs_address = view.findViewById(R.id.bs_addressTv);
+        bs_photo = view.findViewById(R.id.bs_photoIv);
+        bs_address = view.findViewById(R.id.bs_locationEt);
         bs_title = view.findViewById(R.id.bs_titleEt);
         bs_cont = view.findViewById(R.id.bs_contEt);
         imageView = view.findViewById(R.id.bs_photoIv);
 
+        bs_checkBtn = view.findViewById(R.id.bs_checkBtn);
+        bs_locationBtn = view.findViewById(R.id.bs_locationBtn);
 
 
-        bs_mkboard.setOnClickListener(new View.OnClickListener() {
+
+        bs_checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "확인", Toast.LENGTH_SHORT).show();
