88c066c46d2c87a6d89ba39dcf895464447956d1
Usofl <songjang33@naver.com>
2021-11-29 17:12:35 +0900

    댓글 띄우기

--- app/src/main/java/kr/ac/uc/matzip/view/BoardListAdapter.java
+++ app/src/main/java/kr/ac/uc/matzip/view/BoardListAdapter.java
@@ -2,6 +2,7 @@ package kr.ac.uc.matzip.view;
 
 import static kr.ac.uc.matzip.view.FileUtils.TAG;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -14,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
@@ -22,9 +24,11 @@ import java.util.List;
 
 import kr.ac.uc.matzip.R;
 import kr.ac.uc.matzip.model.BoardListModel;
+import kr.ac.uc.matzip.model.CommentListModel;
 import kr.ac.uc.matzip.model.LoveModel;
 import kr.ac.uc.matzip.model.PhotoModel;
 import kr.ac.uc.matzip.presenter.ApiClient;
+import kr.ac.uc.matzip.presenter.CommentAPI;
 import kr.ac.uc.matzip.presenter.LoveAPI;
 import kr.ac.uc.matzip.presenter.PhotoAPI;
 import retrofit2.Call;
@@ -34,12 +38,14 @@ import retrofit2.Response;
 public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.CustomViewHolder> {
 
     private Context context;
-    private ArrayList<BoardListModel> arraylist;
+    private ArrayList<BoardListModel> Board_Arraylist;
+    private ArrayList<CommentListModel> Comment_ArrayList;
     private ArrayList<Uri> imageList;
+    private LinearLayoutManager mLinearLayoutManager;
 
     public BoardListAdapter(Context context,ArrayList<BoardListModel> arraylist) {
         this.context = context;
-        this.arraylist = arraylist;
+        this.Board_Arraylist = arraylist;
     }
 
     @NonNull
@@ -55,19 +61,20 @@ public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.Cust
     @Override
     public void onBindViewHolder(@NonNull BoardListAdapter.CustomViewHolder holder, int position) {
         final int mPosition = position;
-        holder.iig_idTv.setText(arraylist.get(mPosition).getNickname());
-        holder.iig_idTv2.setText(arraylist.get(mPosition).getNickname());
-        holder.iig_titleTv.setText(arraylist.get(mPosition).getBo_title());
-        holder.iig_contIv.setText(arraylist.get(mPosition).getBo_cont());
+        holder.iig_idTv.setText(Board_Arraylist.get(mPosition).getNickname());
+        holder.iig_idTv2.setText(Board_Arraylist.get(mPosition).getNickname());
+        holder.iig_titleTv.setText(Board_Arraylist.get(mPosition).getBo_title());
+        holder.iig_contIv.setText(Board_Arraylist.get(mPosition).getBo_cont());
 
-        select_photo(holder, arraylist.get(mPosition).getBoard_id());
+        select_photo(holder, Board_Arraylist.get(mPosition).getBoard_id());
+        getCommentList(holder, Board_Arraylist.get(mPosition).getBoard_id());
 
         Intent comment_intent = new Intent(context, CommentActivity.class);
 
         holder.iig_commentTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                comment_intent.putExtra("board_id", arraylist.get(mPosition).getBoard_id());
+                comment_intent.putExtra("board_id", Board_Arraylist.get(mPosition).getBoard_id());
                 context.startActivity(comment_intent);
             }
         });
@@ -75,7 +82,7 @@ public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.Cust
         holder.iig_commentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                comment_intent.putExtra("board_id", arraylist.get(mPosition).getBoard_id());
+                comment_intent.putExtra("board_id", Board_Arraylist.get(mPosition).getBoard_id());
                 context.startActivity(comment_intent);
             }
         });
@@ -83,19 +90,20 @@ public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.Cust
         holder.iig_heartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loved_board(arraylist.get(mPosition).getBoard_id());
+                loved_board(Board_Arraylist.get(mPosition).getBoard_id());
             }
         });
     }
 
     @Override
     public int getItemCount() {
-        return (null != arraylist ? arraylist.size() : 0);
+        return (null != Board_Arraylist ? Board_Arraylist.size() : 0);
     }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder {
 
         protected ImageView iig_profileIv;
+        protected RecyclerView iig_commentRv;
         protected ViewPager iig_photoVP;
         protected TextView iig_idTv,iig_titleTv,iig_idTv2,iig_contIv,iig_commentTv,iig_likeTv;
         protected Button iig_heartBtn,iig_commentBtn;
@@ -112,6 +120,7 @@ public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.Cust
             this.iig_likeTv = (TextView) itemView.findViewById(R.id.iig_likeTv);
             this.iig_heartBtn = (Button) itemView.findViewById(R.id.iig_heartBtn);
             this.iig_commentBtn = (Button) itemView.findViewById(R.id.iig_commentBtn);
+            this.iig_commentRv = (RecyclerView) itemView.findViewById(R.id.iig_commentRv);
         }
     }
 
@@ -144,6 +153,36 @@ public class BoardListAdapter extends RecyclerView.Adapter<BoardListAdapter.Cust
             @Override
             public void onFailure(Call<List<PhotoModel>> call, Throwable t) {
                 Log.e(TAG, "select_photo onFailure: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getCommentList(@NonNull BoardListAdapter.CustomViewHolder holder, int board_id) {
+        CommentAPI commentAPI = ApiClient.getNoHeaderApiClient().create(CommentAPI.class);
+        commentAPI.getCommentList(board_id).enqueue(new Callback<List<CommentListModel>>() {
+            @Override
+            public void onResponse(Call<List<CommentListModel>> call, Response<List<CommentListModel>> response) {
+                List<CommentListModel> commentList = response.body();
+
+                Log.d(ContentValues.TAG, "getCommentList: " + commentList.size());
+
+                if(commentList.size() != 0) {
+                    Comment_ArrayList = new ArrayList<>();
+
+                    mLinearLayoutManager = new LinearLayoutManager(context);
+
+                    holder.iig_commentRv.setLayoutManager(mLinearLayoutManager);
+
+                    holder.iig_commentRv.setAdapter(new CommentAdapter(context, Comment_ArrayList));
+
+                    for (int i = 0; i < 1; ++i) {
+                        Comment_ArrayList.add(commentList.get(i));
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<List<CommentListModel>> call, Throwable t) {
+                Log.e(ContentValues.TAG, "Set Board onFailure: " + t.getMessage());
             }
         });
     }
