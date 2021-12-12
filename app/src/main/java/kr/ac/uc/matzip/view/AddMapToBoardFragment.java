925b6c56f58c470a833afc1d00096fdd819fb896
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2021-12-13 01:38:27 +0900

    일단커밋

--- app/src/main/java/kr/ac/uc/matzip/view/AddMapToBoardFragment.java
+++ app/src/main/java/kr/ac/uc/matzip/view/AddMapToBoardFragment.java
@@ -1,12 +1,9 @@
 package kr.ac.uc.matzip.view;
 
-import static android.view.View.inflate;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import static net.daum.mf.map.api.MapPoint.mapPointWithGeoCoord;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,8 +15,6 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -30,8 +25,6 @@ import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
-
-import java.util.concurrent.Executor;
 
 import kr.ac.uc.matzip.R;
 
@@ -50,6 +43,11 @@ public class AddMapToBoardFragment extends Fragment implements MapView.CurrentLo
 
     private MapPoint makerPoint;
     private FusedLocationProviderClient fusedLocationClient;    //위치 정보 가져오기
+
+    public static AddMapToBoardFragment newInstance() {
+        AddMapToBoardFragment addMapToBoardFragment = new AddMapToBoardFragment();
+        return addMapToBoardFragment;
+    }
 
 
     @SuppressLint("ResourceType")
@@ -94,17 +92,6 @@ public class AddMapToBoardFragment extends Fragment implements MapView.CurrentLo
 //            // for ActivityCompat#requestPermissions for more details.
 //            return;
 //        }
-
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return null;
-        }
 
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
